9cb54d4aa9e852d6626004b387fbd945344e7eb6
Utkarsh Sharma <f20160600@Goa.bits-pilani.ac.in>
2019-06-01 03:55:15 -0700

    Add diff attribute
    Adds diff attribute which allows user to
    choose only to show addresses where bytes
    have changed (only for hexdump outformat)

--- multidiff/Render.py
+++ multidiff/Render.py
@@ -1,9 +1,10 @@
 from multidiff.Ansi import Ansi
 import binascii
 import html
+import textwrap
 
 class Render():
-	def __init__(self, encoder='hexdump', color='ansi'):
+	def __init__(self, encoder='hexdump', color='ansi', width=None):
 		'''Configure the output encoding and coloring method of this rendering object'''
 		if   color == 'ansi':
 			self.highligther = ansi_colored
@@ -16,18 +17,35 @@ class Render():
 			self.encoder = HexEncoder
 		elif encoder == 'utf8':
 			self.encoder = Utf8Encoder
-			
+
+		self.width = width
+
 	def render(self, model, diff):
 		'''Render the diff in the given model into a UTF-8 String'''
 		result = self.encoder(self.highligther)
 		obj = model.objects[diff.target]
 		for op in diff.opcodes:
 			data = obj.data[op[3]:op[4]]
+			data2 = ""
 			if type(data) == bytes:
-				result.append(data, op[0])
+				result.append(data, op[0], self.width, data2 )
 			elif type(data) == str:
-				result.append(bytes(data, "utf8"), op[0])
-		return result.final()
+				result.append(bytes(data, "utf8"), op[0], self.width, bytes(data2, "utf8"))
+		return result.final(data2)
+
+	def diff_render(self, model, diff):
+		'''Render the smaller diff in the given model into a UTF-8 String '''
+		result = self.encoder(self.highligther)
+		obj1 = model.objects[diff.target-1]
+		obj2 = model.objects[diff.target]
+		for op in diff.opcodes:
+			data1 = obj1.data[op[1]:op[2]]
+			data2 = obj2.data[op[3]:op[4]]
+			if type(data2) == bytes:
+				result.append(data1, op[0], self.width, data2)
+			elif type(data2) == str:
+				result.append(bytes(data1, "utf8"), op[0], self.width, bytes(data2, "utf8"))
+		return result.final(data2).rstrip()
 
 	def dumps(self, model):
 		'''Dump all diffs in a model. Mostly good for debugging'''
@@ -42,10 +60,13 @@ class Utf8Encoder():
 		self.highligther = highligther
 		self.output = ''
 
-	def append(self, data, color):
+	def append(self, data, color, width=None, data2=""):
 		self.output += self.highligther(str(data, 'utf8'), color)
-	
-	def final(self):
+		if width:
+			if len(self.output) > int(width):
+				self.output = textwrap.fill(self.output, int(width))
+
+	def final(self, data):
 		return self.output
 
 class HexEncoder():
@@ -53,11 +74,15 @@ class HexEncoder():
 	def __init__(self, highligther):
 		self.highligther = highligther
 		self.output = ''
-	def append(self, data, color):
+
+	def append(self, data, color, width=None, data2=""):
 		data = str(binascii.hexlify(data),'utf8')
 		self.output += self.highligther(data, color)
-	
-	def final(self):
+		if width:
+			if len(self.output) > int(width):
+				self.output = textwrap.fill(self.output, int(width))
+
+	def final(self, data):
 		return self.output
 
 class HexdumpEncoder():
@@ -71,46 +96,86 @@ class HexdumpEncoder():
 		self.skipspace = False
 		self.asciirow = ''
 
-	def append(self, data, color):
-		if len(data) == 0:
-			self._append(data, color)
-		while len(data) > 0:
-			if self.rowlen == 16:
-				self._newrow()
-			consumed = self._append(data[:16 - self.rowlen], color)
-			data = data[consumed:]
-
-	def _append(self, data, color):
-		if len(data) == 0:
-			#in the case of highlightig a deletion in a target or an
-			#addition in the source, print a highlighted space and mark
-			#it skippanble for the next append
-			hexs = ' ' 
-			self.skipspace = True
+	def append(self, data, color, width=None, data2=""):
+		if data2 == "":
+			if len(data) == 0:
+				self._append(data, data2, color, width)
+			while len(data) > 0:
+				if self.rowlen == 16:
+					self._newrow(data2)
+				consumed = self._append(data[:16 - self.rowlen], data2, color, width)
+				data = data[consumed:]
 		else:
-			self._add_hex_space()
-			#encode to hex and add some spaces
-			hexs = str(binascii.hexlify(data), 'utf8')
-			hexs = ' '.join([hexs[i:i+2] for i in range(0, len(hexs), 2)])
-			asciis = ''
-			#make the ascii dump
-			for byte in data:
-				if 0x20 <= byte <= 0x7E:
-					asciis += chr(byte)
-				else:
-					asciis += '.'
-			self.asciirow += self.highligther(asciis, color)
+			data1 = data
+			if len(data2) == 0:
+				self._append(data1, data2, color, width)
+			while len(data2) > 0:
+				if self.rowlen == 16:
+					self._newrow(data2)
+				consumed = self._append(data1[:16 - self.rowlen], data2[:16 - self.rowlen], color, width)
+				data2 = data2[consumed:]
 
-		self.hexrow   += self.highligther(hexs, color)
-		self.rowlen += len(data)
-		return len(data)
+	def _append(self, data, data2, color, width):
+		if data2 == "":
+			if len(data) == 0:
+				#in the case of highlightig a deletion in a target or an
+				#addition in the source, print a highlighted space and mark
+				#it skippanble for the next append
+				hexs = ' '
+				self.skipspace = True
+			else:
+				self._add_hex_space()
+				#encode to hex and add some spaces
+				hexs = str(binascii.hexlify(data), 'utf8')
+				hexs = ' '.join([hexs[i:i+2] for i in range(0, len(hexs), 2)])
+				asciis = ''
+				#make the ascii dump
+				for byte in data:
+					if 0x20 <= byte <= 0x7E:
+						asciis += chr(byte)
+					else:
+						asciis += '.'
+				self.asciirow += self.highligther(asciis, color)
 
-	def _newrow(self):
+			self.hexrow += self.highligther(hexs, color)
+			if width:
+				if len(self.hexrow) > int(width):
+					self.hexrow = textwrap.fill(self.hexrow, int(width))
+			self.rowlen += len(data)
+			return len(data)
+		else:
+			data1 = data
+			# <deletion>
+			if len(data2) == 0:
+				hexs = str(binascii.hexlify(data1), 'utf8')
+				hexs = ' '.join([hexs[i:i+2] for i in range(0, len(hexs), 2)])
+			else:
+				self._add_hex_space()
+				#encode to hex and add some spaces
+				hexs = str(binascii.hexlify(data2), 'utf8')
+				hexs = ' '.join([hexs[i:i+2] for i in range(0, len(hexs), 2)])
+
+			self.hexrow += self.highligther(hexs, color)
+			if width:
+				if len(self.hexrow) > int(width):
+					self.hexrow = textwrap.fill(self.hexrow, int(width))
+			self.rowlen += len(data2)
+			return len(data2)
+
+	def _newrow(self, data):
 		self._add_hex_space()
-		if self.addr != 0:
-			self.body += '\n'
-		self.body += "{:06x}:{:s}|{:s}|".format(
-			self.addr, self.hexrow, self.asciirow);
+		ops = ['insert', 'delete', 'replace', Ansi.delete, Ansi.replace, Ansi.insert]
+		if data == "":
+			if self.addr != 0:
+				self.body += '\n'
+			self.body += "{:06x}:{:s}|{:s}|".format(
+				self.addr, self.hexrow, self.asciirow);
+		else:
+			if self.addr != 0:
+				self.body = self.body
+			if any(ext in self.hexrow for ext in ops):
+				self.body += "{:06x}:{:s}\n".format(
+				self.addr, self.hexrow);
 		self.addr += 16
 		self.rowlen = 0
 		self.hexrow = ''
@@ -121,12 +186,11 @@ class HexdumpEncoder():
 			self.skipspace = False
 		else:
 			self.hexrow += ' '
-		
 
-	def final(self):
+	def final(self, data=""):
 		self.hexrow += 3*(16 - self.rowlen) * ' '
 		self.asciirow += (16 - self.rowlen) * ' '
-		self._newrow()
+		self._newrow(data)
 		return self.body
 
 def ansi_colored(string, op):
--- multidiff/command_line_interface.py
+++ multidiff/command_line_interface.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 import argparse
 from multidiff import MultidiffModel, StreamView, SocketController, FileController, StdinController
+import shutil
+import sys
 
-def main():
-	args = make_parser().parse_args()
+def main(args=None):
+
+	if args is None:
+		args = sys.argv[1:]
+	args = make_parser().parse_args(args)
 	m = MultidiffModel()
-	v = StreamView(m, encoding=args.outformat, mode=args.mode, color=args.color)
-	
+
+	if args.width == 'max':
+		args.width = get_max_width(args)
+
+	v = StreamView(m, encoding=args.outformat, mode=args.mode, color=args.color, width=args.width, diff=args.diff)
+
 	if len(args.file) > 0:
 		informat = args.informat if args.informat else 'raw'
 		files = FileController(m, informat)
@@ -19,6 +28,11 @@ def main():
 		informat = args.informat if args.informat else 'raw'
 		server = SocketController(('127.0.0.1', args.port), m, informat)
 		server.serve_forever()
+
+def get_max_width(args):
+	columns = int(shutil.get_terminal_size((120,30)).columns)
+	args.width = columns
+	return args.width
 
 def make_parser():
 	parser = argparse.ArgumentParser(
@@ -75,6 +89,16 @@ def make_parser():
 		const='html',
 		default='ansi',
 		help='use html for colors instead of ansi codes')
+
+	parser.add_argument('-w', '--width',
+		dest='width',
+		default='82',
+		help='number of bytes printed per line, either an integer or max(width of console)')
+
+	parser.add_argument('-d', '--diff',
+		dest='diff',
+		action='store_true',
+		help='only show addresses where bytes have changed(only for hexdump outformat)')
 	return parser
 
 if __name__ == '__main__':
